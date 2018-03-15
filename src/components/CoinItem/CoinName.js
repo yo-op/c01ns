a333371c9cd999e42d3226590a99b32b4220ee39
oparenko <yuriy@oparenko.com>
2018-03-15 22:21:19 +0000

    Refactor

--- src/components/CoinItem/CoinName.js
+++ src/components/CoinItem/CoinName.js
@@ -1,41 +1,40 @@
 import React, { Component, } from 'react';
-import { Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 
 const CoinFullName = styled.Text`
-    font-size: 16px;
-    line-height: 19px;
-    color: white;
-    margin-bottom: 2px;
+  font-size: 16px;
+  line-height: 19px;
+  color: white;
+  margin-bottom: 2px;
 `;
 
 const CoinShortName = styled.Text`
-    font-size: 14px;
-    line-height: 16px;
-    color: rgba(255,255,255, 0.5);
+  font-size: 14px;
+  line-height: 16px;
+  color: rgba(255,255,255, 0.5);
 `;
 
 const CoinNameWrapper = styled.View`
-    display: flex;
-    flex-direction: column; 
+  display: flex;
+  flex-direction: column;
 `;
 
 class CoinName extends Component {
 
-    static propTypes = {
-        coinShortName: PropTypes.string,
-        coinFullName: PropTypes.string,
-    };
+  static propTypes = {
+    coinShortName: PropTypes.string,
+    coinFullName: PropTypes.string,
+  };
 
-    render() {
-        return(
-            <CoinNameWrapper>
-                <CoinFullName>{this.props.coinFullName}</CoinFullName>
-                <CoinShortName>{this.props.coinShortName}</CoinShortName>
-            </CoinNameWrapper>
-        );
-    }
+  render() {
+    return(
+      <CoinNameWrapper>
+        <CoinFullName>{this.props.coinFullName}</CoinFullName>
+        <CoinShortName>{this.props.coinShortName}</CoinShortName>
+      </CoinNameWrapper>
+    );
+  }
 }
 
 export default CoinName;
